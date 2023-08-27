ba00197405528e0ad11f21a04d7d4b2779f396eb
mushvig95 <mushvig.niftaliev@Mushvigs-MacBook-Pro.local>
2023-08-27 23:50:22 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MarketingApp from './components/mf/Marketing';
+
+export default () => {
+    return <div>
+        <h1>THIS IS CONTAINER</h1>
+        <MarketingApp />
+        </div>
+}
